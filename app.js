c07deeee1ec1d453009f8ec5e1e6ea769df088a5
Adina Shanholtz <adinas@microsoft.com>
2017-05-31 11:55:42 -0400

    got op dialog

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
+//=========================================================
+// Bot Setup
+//=========================================================
+
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -15,8 +19,49 @@ var connector = new builder.ChatConnector({
 
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
+var bot = new builder.UniversalBot(connector);
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-});
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+
+/* Beginning of the game dialog */
+bot.dialog('/', [
+    function (session, results) {
+        session.send('Hello?');
+        builder.Prompts.text(session, 'Oh! Hi! I wasn\'t sure if the translation software was working. What\'s your name?');
+    },
+    function (session, results) {
+        builder.Prompts.text(session, 'Hi ' +  results.response + '! That\'s a cool name. I\'m not sure if you could pronounce my real name, but you can call me Kh\'atyan.');
+    },
+    function (session, results) {
+        if(results.response.toLowerCase().includes('nay\'a ionthae')){
+            session.beginDialog('/endGame');
+            session.endDialog();
+        }
+        else{
+            session.beginDialog('/restOfGame');
+            session.endDialog();
+        }
+
+    }
+]);
+
+
+/* ENDGAME */
+bot.dialog('/endGame', [
+    function (session) {
+        session.send('This is the endgame');
+    }
+]);
+
+bot.dialog('/restOfGame', [
+    function (session) {
+        builder.Prompts.text(session, 'This is the rest of game');
+    },
+    function (session, results) {
+        session.endDialogWithResult(results);
+    }
+]);
